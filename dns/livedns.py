4e79f1116e2b6fcdc6496953db1ed42082243b0b
Alexandro Sanchez Bach <alexandro@phi.nz>
2022-12-28 13:33:22 +0100

    Fix bug with non-A records

--- dns/livedns.py
+++ dns/livedns.py
@@ -9,7 +9,7 @@ def set_dns_records(ip, key, entries):
 
     for e in entries:
         record = requests.utils.quote(e.record)
-        response = requests.put(f'{base}/livedns/domains/{e.domain}/records/{record}/A', headers=auth,
+        response = requests.put(f'{base}/livedns/domains/{e.domain}/records/{record}/{e.type}', headers=auth,
             json={ "rrset_values": e.data, "rrset_ttl": 300 })
         if response.status_code != 201:
             raise Exception(f"Could not update {e.type} record {e.record}.{e.domain}.")
